6e1cae578b6c31d0b0c1909bff904269333a2a02
Paul Krush <pkrush@gemhunt.com>
2017-08-07 12:11:29 -0500

    Working on template matching

--- screws-temp.py
+++ screws-temp.py
@@ -27,13 +27,9 @@ def get_thresh(img,flip):
     return img
 
 def get_contours(img):
-    # min_area = 910
-    # max_area = 1240
-    # max_area = 1800
-
-    min_area = 230
-    max_area = 350
-
+    min_area = 910
+    max_area = 1240
+    max_area = 1800
     size_factor = 1
     #small = cv2.resize(im,(int(1920 *size_factor) ,int(1080 * size_factor)))
     #img = small.copy()
@@ -51,76 +47,11 @@ def get_contours(img):
             else:
                 if 100 < area:
                     contours_filtered_out.append(cnt)
-                    print area
-
         count +=1
     #cv2.drawContours(img,filtered_contours,-1,255,1)
     #cv2.imshow("Contours",img)
     #cv2.waitKey(0)
     return contours_filtered_in,contours_filtered_out
-
-
-from matplotlib import pyplot as plt
-#Use template matching to break apart contours that watershed can't
-def break_contours(contours,frame,template_contour):
-    broken_contours = []
-    max_area = 1800
-    x, y, w, h = cv2.boundingRect(template_contour)
-    template = frame[y:y + h,x:x+w]
-    start_time = time.time()
-
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        if area > max_area:
-            x, y, w, h = cv2.boundingRect(cnt)
-            #print w, h
-
-            img_to_search = frame[y:y + h, x:x + w]
-            img3 = np.zeros((h+100,w+100), dtype=np.uint8)
-            img3 = img3 + 255
-            img3[50:h+50,50:w+50] = img_to_search
-            img2 = img3.copy()
-
-            #img2 = frame.copy()
-            #cv2.imshow('img3', img3)
-            #cv2.imshow('img_to_search', img_to_search)
-            #cv2.imshow('template', template)
-            #cv2.waitKey(1)
-
-            w, h = template.shape[::-1]
-
-            # All the 6 methods for comparison in a list
-            methods = ['cv2.TM_CCOEFF']
-
-            for meth in methods:
-                img = img2.copy()
-                method = eval(meth)
-
-                # Apply template Matching
-
-                res = cv2.matchTemplate(img, template, method)
-
-                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
-                # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-                if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-                    top_left = min_loc
-                else:
-                    top_left = max_loc
-                bottom_right = (top_left[0] + w, top_left[1] + h)
-
-                cv2.rectangle(img, top_left, bottom_right, 128, 2)
-
-                #if max_val > 40075832.0:
-                # plt.subplot(121), plt.imshow(res, cmap='gray')
-                # plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-                # plt.subplot(122), plt.imshow(img, cmap='gray')
-                # plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-                # plt.suptitle(meth)
-                # plt.show()
-    print 'Done in %s seconds' % (time.time() - start_time,)
-
-    return broken_contours
 
 def get_features(contours):
     all_features = []
@@ -152,19 +83,23 @@ def get_features(contours):
     return all_features
 
 start_time = time.time()
-dir = '/home/pkrush/find-parts-faster-data/screws/2/'
+dir = '/home/pkrush/find-parts-faster-data/screws/0/'
 contours = []
 for filename in os.listdir(dir):
     im = cv2.imread(dir + filename, cv2.IMREAD_GRAYSCALE)
     im = get_crop(im)
+
+    # im = get_thresh(im,True)
+    # kernel = np.ones((3, 3), np.uint8)
+    # im = cv2.erode(im, kernel, iterations=)
+    # #im = cv2.dilate(im, kernel, iterations=1)
+
+    # gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
     contours_filtered_in, contours_filtered_out = get_contours(im)
-
-    #I need to create an average one instead? contours_filtered_in[0]
-    #broken_contours= break_contours(contours_filtered_out,im,contours_filtered_in[0])
-
     contours.extend(contours_filtered_in)
 count_of_good_contours = len(contours)
-dir = '/home/pkrush/find-parts-faster-data/screws/3/'
+dir = '/home/pkrush/find-parts-faster-data/screws/1/'
 for filename in os.listdir(dir):
     im = cv2.imread(dir + filename, cv2.IMREAD_GRAYSCALE)
     im = get_crop(im)
@@ -179,7 +114,7 @@ all_features = get_features(contours)
 
 names = ["Nearest Neighbors", "Gaussian Process",
          "Random Forest", "Neural Net", "AdaBoost",
-         "Naive Bayes","RBF SVM","QDA","Decision Tree"]
+         "Naive Bayes","Decision Tree", "RBF SVM","QDA"]
 
 classifiers = [
     KNeighborsClassifier(3),
@@ -188,9 +123,9 @@ classifiers = [
     MLPClassifier(alpha=1),
     AdaBoostClassifier(),
     GaussianNB(),
+    DecisionTreeClassifier(max_depth=5),
     SVC(gamma=2, C=1),
-    QuadraticDiscriminantAnalysis(),
-    DecisionTreeClassifier(max_depth=5)]
+    QuadraticDiscriminantAnalysis()]
 
 
 
@@ -226,7 +161,7 @@ for name, clf in zip(names, classifiers):
 # is more influential
 #print(np.std(X, 0)*logistic.coef_)
 
-#sys.exit()
+sys.exit()
 
 cap = cv2.VideoCapture(0)
 cap.set(3,1920)
@@ -262,7 +197,6 @@ for x in range(0,400000):
     background[:, :] = (255, 255, 255)
 
     cv2.drawContours(background, good_contours, -1, (150,150,255), -5)
-    cv2.drawContours(background, bad_contours, -1, (220, 255, 220), 15)
     cv2.drawContours(background, bad_contours, -1, (150, 255 ,150), -5)
     cv2.drawContours(background, contours_filtered_out, -1, (255, 200, 200), -5)
 
